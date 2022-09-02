b1f555dd788e5cf621ac6b5fbe2d143fe4c65e49
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-09-03 00:23:28 +0500

    work done

--- server.mjs
+++ server.mjs
@@ -1,162 +1,129 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors'
+import cors from 'cors';
+import mongoose from 'mongoose'
 
-const productSchema = new mongoose.Schema({
-    productName: String,
-    productPrice: Number,
-    currencyCode: String,
-    numberOfSale: Number,
-    rating: Number,
-    isFreeShipping: Boolean,
-    shopName: String,
-    createdOn: { type: Date, default: Date.now },
-});
-const productModel = mongoose.model('Product', productSchema);
-
-let app = express();
+const app = express()
+const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
 
-app.get("/products", async (req, res) => {
 
-    let result = await productModel
-        .find({})
-        .exec()
-        .catch(e => {
-            console.log("error in db: ", e);
-            res.status(500).send({ message: "error in getting all products" });
-            return
-        })
+const productSchema = new mongoose.Schema({
+    productName: { type: String },
+    productPrice: { type: Number },
+    currencyCode: { type: String },
+    numberOfSale: { type: Number },
+    rating: { type: Number },
+    isFreeShipping: { type: Boolean },
+    shopName: { type: String },
+    createdOn: { type: Date, default: Date.now }
 
-    res.send({
-        message: "all products success ",
-        data: result
-    });
 });
-
-app.get("/product/:id", async (req, res) => {
-
-    let result = await productModel
-        .findOne({_id: req.params.id})
-        .exec()
-        .catch(e => {
-            console.log("error in db: ", e);
-            res.status(500).send({ message: "error in getting all products" });
-            return
-        })
-
-    res.send({
-        message: "all products success ",
-        data: result
-    });
-});
+const productModel = mongoose.model('product', productSchema);
 
 
-app.post("/product", async (req, res) => {
-
+app.post('/product', (req, res) => {
     let body = req.body;
-
     if (
-        !body.productName
-        || !body.productPrice
-        || !body.currencyCode
-        || !body.numberOfSale
-        || !body.rating
-        || !body.isFreeShipping
-        || !body.shopName
-    ) {
+        !body.productName ||
+        !body.productPrice ||
+        !body.currencyCode ||
+        !body.numberOfSale ||
+        !body.rating ||
+        !body.isFreeShipping ||
+        !body.shopName
 
+    ) {
+        console.log("please fill all fields")
         res.status(400).send({
-            message: `required field missing, all fields are required: 
-            productName
-            productPrice
-            currencyCode
-            numberOfSale
-            rating
-            isFreeShipping
-            shopName`
+            message: "please fill all fields"
         })
         return;
     }
 
-    let result = await productModel.create({
 
+
+
+
+    let newproduct = new productModel({
         productName: body.productName,
         productPrice: body.productPrice,
         currencyCode: body.currencyCode,
         numberOfSale: body.numberOfSale,
         rating: body.rating,
         isFreeShipping: body.isFreeShipping,
-        shopName: body.shopName,
-
-    }).catch(e => {
-        console.log("error in db: ", e);
-        res.status(500).send({ message: "db error in saving product" });
+        shopName: body.shopName
     })
 
-    console.log("result: ", result);
-    res.send({ message: "product is added in database" });
-});
+    newproduct.save((err, result) => {
 
-app.delete("/product/:id", async (req, res) => {
+        if (!err) {
+            res.send({
+                message: "product is created"
+            })
+            console.log(result, "product is created in database")
+        } else {
+            res.send({ message: "db error in saving product" });
+            console.log()
 
-    let _id = req.params.id;
-
-    try {
-        const result = await productModel.findByIdAndDelete(_id);
-        console.log("Deleted product: ", result);
-        res.send({
-            message: "deleted"
-        });
-        return;
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({
-            message: "db error"
-        })
-    }
+        }
 
 
 
-})
-app.put("/product/:id", async (req, res) => {
-
-    let _id = req.params.id;
-    let body = req.body;
-
-    try {
-        const result = await productModel.findByIdAndUpdate(_id, body);
-        console.log("updated product: ", result);
-        res.send({
-            message: "updated"
-        });
-        return;
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({
-            message: "db error"
-        })
-    }
+    })
 })
 
-// app.use("/*", (req, res) => {
-//     console.log(" I am * handler");
-//     res.status(404).send("this api doesn't exist");
-// })
+
+app.get("/products", (req, res) => {
+    productModel.find({}, (err, result) => {
+        if (err) {
+            res.send({
+                message: "error in getting all products"
+            })
+            console.log(err, "error in db")
+            return;
+        } else {
+            res.send({
+                message: "got all products",
+                data: result
+            })
+        }
+    }
+ )
+})
 
 
-let PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`app is running on ${PORT}`);
+app.delete("/product/:id", (req, res) => {
+    let id = req.params.id;
+    
+
+    productModel.findByIdAndDelete(id, function (err, result) {
+        if (err) {
+            console.log(err)
+            res.send({
+                message : "error in db"
+            })
+            return;
+        }
+        else {
+            console.log("Deleted product: ", result);
+            res.send({
+                message : "deleted"
+            })
+
+            return;
+        }
+    });
+
 });
 
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
+
 let dbURI = 'mongodb+srv://abc:abc@cluster0.iunhwh0.mongodb.net/ECommerceDatabase?retryWrites=true&w=majority';
 mongoose.connect(dbURI);
 
